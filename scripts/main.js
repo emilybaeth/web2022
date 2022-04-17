589c592592f4b0bf9248a4a6c0b0c6a12c0b569f
Emily <eeba242@uky.edu>
2022-04-17 15:37:33 -0400

    adding ID's

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,24 @@ if (new Date().getHours() > 13) {
   html.style.backgroundColor = 'black';
   html.style.color = 'white';
 }
+
+if (new Date().getHours() > 13) {
+  const content = document.querySelector('section');
+  section.style.backgroundColor = 'black';
+  section.style.color = 'white';
+}
+
+if (new Date().getHours() > 13) {
+  const header = document.querySelector('header');
+  header.style.color = 'black';
+}
+
+if (new Date().getHours() > 13) {
+  const footer = document.querySelector('footer');
+  footer.style.color = 'black';
+}
+
+if (new Date().getHours() > 13) {
+  const footer = document.querySelector('footer');
+  footer.style.color = 'black';
+}
